5a7b678b33bf3901a1322862b1eece1034cb7c80
Shabib Mohammed <uniqueb7h@gmail.com>
2023-02-03 09:35:34 +0300

    header

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -2,9 +2,24 @@
  * rnf: React Native Functional componenet
  */
 
-import { View, Text, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import {
+  HomeModernIcon,
+  ChevronDownIcon,
+} from "react-native-heroicons/outline";
+import {
+  MagnifyingGlassIcon,
+  AdjustmentsHorizontalIcon,
+} from "react-native-heroicons/solid";
 
 export default function HomeScreen() {
   const nav = useNavigation();
@@ -17,22 +32,42 @@ export default function HomeScreen() {
 
   return (
     // exlcude the upper borders
-    <SafeAreaView>
-      <View className="flex-row mx-5 items-center  justify-between">
-        {/* Header */}
-        <View className="">
-          <Image
-            source={{
-              uri: "https://links.papareact.com/wru",
-            }}
-            className="h-10 w-10 bg-green-300 rounded-full"
-          />
+    <SafeAreaView className="bg-white">
+      {/* Header */}
+      <View className="flex-row mx-3 pb-3">
+        <View className="flex-1  ">
+          <TouchableOpacity className="flex-row items-center space-x-1">
+            <Image
+              source={{
+                uri: "https://links.papareact.com/wru",
+              }}
+              className="h-10 w-10 bg-green-300 rounded-full"
+            />
+            <ChevronDownIcon size={20} color="grey" />
+          </TouchableOpacity>
         </View>
 
-        <View className="items-center">
-          <Text className="text-blue-500 font-bold text-xl">HomeScreen</Text>
-          <Text className="text-xs">Deliver now !</Text>
+        <View className="space-x-4 items-end">
+          <Text className="font-bold text-xl">Current Location</Text>
+          <Text className="text-xs text-blue-300 font-bold">
+            Delivering now !
+          </Text>
         </View>
+      </View>
+
+      {/* Search */}
+      <View className="flex-row space-x-2 items-center pb-2 mr-2">
+        <View className="flex-row  bg-gray-200 p-3 space-x-2 flex-1">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput
+            keyboardType="default"
+            placeholder="Resturants :P"
+            className="text-sm"
+          ></TextInput>
+        </View>
+        <TouchableOpacity>
+          <AdjustmentsHorizontalIcon />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
